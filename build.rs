7e5ed8c94a2785006c790239dd2ea4e5915a969e
Zeke Sonxx <zeke@zekesonxx.com>
2017-03-06 17:55:25 -0500

    Change the C bindings to be an optional dependancy

--- build.rs
+++ build.rs
@@ -1,5 +1,10 @@
+#[cfg(feature = "cbinding")]
 extern crate gcc;
 
+#[cfg(feature = "cbinding")]
 fn main() {
     gcc::compile_library("libsmaz.a", &["src/smaz.c"]);
 }
+
+#[cfg(not(feature = "cbinding"))]
+fn main() {}
--- src/main.rs
+++ src/main.rs
@@ -3,50 +3,56 @@
 #![allow(unknown_lints)]
 #![feature(test)]
 extern crate test;
-extern crate rand;
-extern crate libc;
+
 #[macro_use] extern crate lazy_static;
+
+#[cfg(feature = "cbinding")]
+extern crate rand;
+#[cfg(feature = "cbinding")]
+extern crate libc;
 
 
 mod port;
+#[cfg(feature = "cbinding")]
 mod cbinding;
 
 
-fn test_string(input: &str) {
-    let bytes = input.as_bytes();
-    let compressed = cbinding::smaz_compress_clean(bytes);
-    let decompressed = cbinding::smaz_decompress_clean(compressed.as_slice());
+//fn test_string(input: &str) {
+//    let bytes = input.as_bytes();
+//    let compressed = cbinding::smaz_compress_clean(bytes);
+//    let decompressed = cbinding::smaz_decompress_clean(compressed.as_slice());
+//
+//    if bytes != decompressed.as_slice() {
+//        println!("ERROR! Mismatch!");
+//        println!("attempting to compress: '{}'", input);
+//        println!("original: {:?}", bytes);
+//        println!("compressed: {:?}", compressed);
+//        println!("decompressed: {:?}", decompressed);
+//        ::std::process::exit(1);
+//    }
+//
+//    // Rust decompressor test
+//    let portdecompressed = port::decompress(compressed.as_slice());
+//    if bytes != portdecompressed.as_slice() {
+//        println!("Port didn't decompress right:");
+//        println!("attempting to compress: '{}'", input);
+//        println!("original: {:?}", bytes);
+//        println!("compressed: {:?}", compressed);
+//        println!("port decompressed: {:?}", portdecompressed);
+//        ::std::process::exit(1);
+//    }
+//
+//
+//    let ratio = 100isize-((100*compressed.len())/bytes.len()) as isize;
+//    if ratio > 0 {
+//        print!("'{}' compressed by {}%", input, ratio);
+//    } else {
+//        print!("'{}' enlarged by {}%", input, -ratio);
+//    }
+//    println!(" ({}B->{}B)", bytes.len(), compressed.len());
+//}
 
-    if bytes != decompressed.as_slice() {
-        println!("ERROR! Mismatch!");
-        println!("attempting to compress: '{}'", input);
-        println!("original: {:?}", bytes);
-        println!("compressed: {:?}", compressed);
-        println!("decompressed: {:?}", decompressed);
-        ::std::process::exit(1);
-    }
-
-    // Rust decompressor test
-    let portdecompressed = port::decompress(compressed.as_slice());
-    if bytes != portdecompressed.as_slice() {
-        println!("Port didn't decompress right:");
-        println!("attempting to compress: '{}'", input);
-        println!("original: {:?}", bytes);
-        println!("compressed: {:?}", compressed);
-        println!("port decompressed: {:?}", portdecompressed);
-        ::std::process::exit(1);
-    }
-
-
-    let ratio = 100isize-((100*compressed.len())/bytes.len()) as isize;
-    if ratio > 0 {
-        print!("'{}' compressed by {}%", input, ratio);
-    } else {
-        print!("'{}' enlarged by {}%", input, -ratio);
-    }
-    println!(" ({}B->{}B)", bytes.len(), compressed.len());
-}
-
+#[cfg(feature = "cbinding")]
 fn main() {
     use std::env;
 
@@ -72,5 +78,25 @@ fn main() {
         println!("         {:?}", String::from_utf8(rustver_de));
 
     }
-    //flame::dump_html(&mut ::std::fs::File::create("flame-graph.html").unwrap()).unwrap();
+}
+
+#[cfg(not(feature = "cbinding"))]
+fn main() {
+    use std::env;
+
+    for argument in env::args() {
+        println!("compressing: '{}'", argument);
+        let rustver = port::compress(argument.as_bytes());
+        println!("----");
+        println!("compressed:");
+        println!("rustver: {:?}", rustver);
+        let ratio = 100isize-((100*rustver.len())/argument.len()) as isize;
+        println!("         {}% compression ({}B->{}B)", ratio, argument.len(), rustver.len());
+        let rustver_de = port::decompress(rustver.as_slice());
+        println!("----");
+        println!("decompressed:");
+        println!("rustver: {:?}", rustver_de);
+        println!("         {:?}", String::from_utf8(rustver_de));
+
+    }
 }
